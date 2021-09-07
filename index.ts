7cb7f895ba90fdc4e18e6c6ea57d18c4f4a8bfc5
Joshua Perez <joshuaperezmelgazo@gmail.com>
2021-09-07 09:31:48 +0800

    Test branch

--- index.ts
+++ index.ts
@@ -14,17 +14,17 @@ const main = async () => {
   const port = (process.env.NODE_ENV === 'local')? 3000 : process.env.PORT;
 
   // auth0 authentication with JWT
-  // const jwtCheck = jwt({
-  //   secret: jwks.expressJwtSecret({
-  //       cache: true,
-  //       rateLimit: true,
-  //       jwksRequestsPerMinute: 5,
-  //       jwksUri: 'https://dev-uy5jvq8p.us.auth0.com/.well-known/jwks.json'
-  //   }),
-  //   audience: 'http://127.0.0.1:3000',
-  //   issuer: 'https://dev-uy5jvq8p.us.auth0.com/',
-  //   algorithms: ['RS256']
-  // });
+  const jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://dev-uy5jvq8p.us.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'http://127.0.0.1:3000',
+    issuer: 'https://dev-uy5jvq8p.us.auth0.com/',
+    algorithms: ['RS256']
+  });
 
   app.use(express.urlencoded({extended: true}));
   app.use(express.json());
@@ -32,22 +32,22 @@ const main = async () => {
 
   // retrieve access token
   // TO DO: set token in authorization header for future requests
-  // const options = {
-  //     method: 'POST',
-  //     url: 'https://dev-uy5jvq8p.us.auth0.com/oauth/token',
-  //     headers: { 'content-type': 'application/json' },
-  //     body: '{"client_id":"j6mGyftrjBS4fa1qbjOSdhQFeUuvm3IG","client_secret":"PANXKyw5KkL3FZ2NwVj8qSgigYGsXywpMcIDAfPaBa7zZC136PW2VikO_t2VQOYL","audience":"http://127.0.0.1:3000","grant_type":"client_credentials"}'
-  // };
-  // request(options, async function (error: string, response: Response, body: string) {
-  //     if (error) {
-  //     throw new Error(error);
-  //     } else {
-  //       logger.info({
-  //         label: 'index.js',
-  //         message: body
-  //       });
-  //     }
-  // });
+  const options = {
+      method: 'POST',
+      url: 'https://dev-uy5jvq8p.us.auth0.com/oauth/token',
+      headers: { 'content-type': 'application/json' },
+      body: '{"client_id":"j6mGyftrjBS4fa1qbjOSdhQFeUuvm3IG","client_secret":"PANXKyw5KkL3FZ2NwVj8qSgigYGsXywpMcIDAfPaBa7zZC136PW2VikO_t2VQOYL","audience":"http://127.0.0.1:3000","grant_type":"client_credentials"}'
+  };
+  request(options, async function (error: string, response: Response, body: string) {
+      if (error) {
+      throw new Error(error);
+      } else {
+        logger.info({
+          label: 'index.js',
+          message: body
+        });
+      }
+  });
 
   app.use('/rest/v1/auth', apis.AuthenticationApi);
   app.use('/rest/v1/user', apis.UserApi);
